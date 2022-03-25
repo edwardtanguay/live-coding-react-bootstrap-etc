6f4c2388e50f4dfa47de00c2a29086e40f5f4fb1
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-25 09:58:49 +0100

    added buttons and container

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import { Button, Container } from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-		  Hallo
-    </div>
-  )
+	return (
+		<div className="App">
+			<Container>
+				<h1>Info</h1>
+				<Button variant="success">Submit</Button>
+				<Button className="ms-2" disabled variant="danger">Delete User</Button>
+				<Button className="ms-2" size="lg" variant="outline-info" >Delete User</Button>
+			</Container>
+		</div>
+	)
 }
 
 export default App
