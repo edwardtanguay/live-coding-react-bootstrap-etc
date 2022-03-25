5690b4412ff60dac94432366270c30a4f65973c5
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-25 11:06:03 +0100

    form has correct fields

--- src/components/InfoForm.jsx
+++ src/components/InfoForm.jsx
@@ -5,20 +5,21 @@ export const InfoForm = () => {
 		<Card className="mt-4">
 			<Card.Body>
 				<Form>
-					<Form.Group className="mb-3" controlId="formBasicEmail">
-						<Form.Label>Email address</Form.Label>
-						<Form.Control type="email" placeholder="Enter email" />
+					<Form.Group className="mb-3">
+						<Form.Label>First Name</Form.Label>
+						<Form.Control type="text" />
 						<Form.Text className="text-muted">
-							We'll never share your email with anyone else.
+							You can also type <code>/id-number</code> to lookup employee.
 						</Form.Text>
 					</Form.Group>
-
-					<Form.Group className="mb-3" controlId="formBasicPassword">
-						<Form.Label>Password</Form.Label>
-						<Form.Control type="password" placeholder="Password" />
+					<Form.Group className="mb-3">
+						<Form.Label>Last Name</Form.Label>
+						<Form.Control type="text" />
 					</Form.Group>
-					<Form.Group className="mb-3" controlId="formBasicCheckbox">
-						<Form.Check type="checkbox" label="Check me out" />
+
+					<Form.Group className="mb-3">
+						<Form.Label>Department</Form.Label>
+						<Form.Control type="text" />
 					</Form.Group>
 					<Button variant="primary" type="submit">
 						Submit
